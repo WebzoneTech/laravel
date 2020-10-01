48816eed48c786e1452b2346e3ba7d8aaa51b2e6
vishnuachu <vishnuachu.cp@gmail.com>
2020-10-01 17:29:03 +0530

    css&js

---
+++ resources/views/Admin/layout/app.blade.php
@@ -0,0 +1,16 @@
+
+	<!-- header -->
+		@include('admin.SuperAdmin.layout.header')
+	<!-- header -->
+
+	<!-- navigation -->
+		@include('admin.SuperAdmin.layout.nav')
+	<!-- navigation -->
+
+	<!-- content -->
+		@yield('content')
+	<!-- content -->
+
+	<!-- footer -->
+		@include('admin.SuperAdmin.layout.footer')
+	<!-- footer -->
